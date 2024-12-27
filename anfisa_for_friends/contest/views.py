769be4297931ffbb484d4f1c513947aedf980609
Роман Кораблев <korabl79@mail.ru>
2024-12-27 10:23:08 +0300

    the ability to edit and delete records is enabled

--- anfisa_for_friends/contest/views.py
+++ anfisa_for_friends/contest/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import ContestForm
+from .models import Contest
 
 
-def proposal(request):
-    form = ContestForm(request.POST or None)
+def proposal(request, pk=None):
+    if pk is not None:
+        instance = get_object_or_404(Contest, pk=pk)
+    else:
+        instance = None
+    form = ContestForm(request.POST or None, instance=instance)
     context = {'form': form}
+    if form.is_valid():
+        form.save()
     return render(request, 'contest/form.html', context=context)
+
+
+def delete_proposal(request, pk):
+    instance = get_object_or_404(Contest, pk=pk)
+    form = ContestForm(instance=instance)
+    context = {'form': form}
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('contest:list')
+    return render(request, 'contest/form.html', context)
+
+
+def proposal_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    contest_proposals = Contest.objects.all().order_by('id')
+    # Передаём их в контекст шаблона.
+    context = {'contest_proposals': contest_proposals}
+    return render(request, 'contest/contest_list.html', context)
